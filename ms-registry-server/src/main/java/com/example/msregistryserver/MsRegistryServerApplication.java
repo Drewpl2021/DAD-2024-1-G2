c48259affa196daf32797fee1f3b37f8dfc1728a
+957365858andres <andres.montes@cata.edu.pe>
2024-04-01 09:19:26 -0500

    asd

--- ms-registry-server/src/main/java/com/example/msregistryserver/MsRegistryServerApplication.java
+++ ms-registry-server/src/main/java/com/example/msregistryserver/MsRegistryServerApplication.java
@@ -3,7 +3,7 @@ package com.example.msregistryserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@En
+@
 @SpringBootApplication
 public class MsRegistryServerApplication {
 
